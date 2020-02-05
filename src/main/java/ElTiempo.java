9c024c14213ca764a39c638596518018ebf6c298
jalvarezbarciela <56076790+jalvarezbarciela@users.noreply.github.com>
2020-02-05 14:39:18 +0100

    Mostrar temperatura

---
+++ src/main/java/ElTiempo.java
@@ -0,0 +1,16 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.IOException;
+
+public class ElTiempo {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").userAgent("Mozilla").get();
+             Elements miElemento=doc.getElementsByClass("today_nowcar-temp");
+             String miTexto=miElemento.text();
+             System.out.println(miTexto);
+    }
+}
